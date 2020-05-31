da80ceef8d3d90cb00817a8a5e8dff10e3deff2b
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-05-31 14:28:08 -0300

    Updates

---
+++ static/js/4.7b6d437c.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpreact-hackaprev"]=this["webpackJsonpreact-hackaprev"]||[]).push([[4],{61:function(n,t,e){"use strict";e.r(t),e.d(t,"BoxDivider",(function(){return r}));var a=e(15);function i(){var n=Object(a.a)(["\n  display: flex;\n  height: 30px;\n  width: 56px;\n  justify-content: center;\n  padding-left: 16px;\n  :last-child {\n    display: none;\n  }\n"]);return i=function(){return n},n}var r=e(16).a.div(i())}}]);
+//# sourceMappingURL=4.7b6d437c.chunk.js.map
