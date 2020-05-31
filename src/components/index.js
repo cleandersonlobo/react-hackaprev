a45f5a0ad766ec8fd868aa2fc576d64f7bb8755b
Cleanderson Lobo <cleandersonlobo@gmail.com>
2020-05-31 14:32:29 -0300

    Updates

--- src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as SuspenseLoading } from './SuspenseLoading/SuspenseLoading';
-export { default as BottomTabsNavigation } from './BottomTabsNavigation/BottomTabsNavigation';
-export { default as Header } from './Header/Header';
